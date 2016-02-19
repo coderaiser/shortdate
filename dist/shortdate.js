b9615fb9a9a4ea5e40c551546a24c493322b9410
coderaiser <mnemonic.enemy@gmail.com>
2016-02-19 09:13:21 -0500

    chore(package) v1.1.0

---
+++ dist/shortdate.js
@@ -0,0 +1,34 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.shortdate = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+module.exports = function(date) {
+    date = date || new Date();
+    
+    check(date);
+     
+    var ret,
+        day     = date.getDate(),
+        month   = date.getMonth() + 1,
+        year    = date.getFullYear();
+        
+    if (month <= 9)
+        month   = '0' + month;
+    
+    if (day <= 9)
+        day     = '0' + day;
+    
+    ret         = year + '.' + month + '.' + day;
+    
+    return ret;
+};
+
+function check(date) {
+    var type = {}.toString.call(date);
+    
+    if (type && type !== '[object Date]')
+        throw Error('date should be Date!');
+ }
+
+
+},{}]},{},[1])(1)
+});
